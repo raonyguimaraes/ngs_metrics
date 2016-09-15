5302cdf44ca2d6615573936ae110af88936c4b98
Raony Guimarães <raonyguimaraes@gmail.com>
2016-09-15 19:50:43 +0200

    vcf-stats exome done

--- vcf_exome_metrics.py
+++ vcf_exome_metrics.py
@@ -37,69 +37,69 @@ snpeff_path = "/home/ubuntu/projects/programs/snpeff/snpEff"
 #/home/ubuntu/projects/programs/vcftools/vcftools-0.1.14/src/cpp/
 #/home/ubuntu/projects/programs/vcftools/vcftools-0.1.14/src/perl/
 
-# #extract vcf from gvcf
-# print('extract vcf from gvcf')
-# #gzip -dc ../../input/WGC081270U.g.vcf.gz | ../../programs/gvcftools-0.16/bin/extract_variants | bgzip -c > WGC081270U.vcf.gz
-# command = """gzip -dc %s | %s/extract_variants | bgzip -c > %s.vcf.gz
-# """ % (vcf_file, gvcftools_path, output_base)
-# output = call(command, shell=True)
-# print(output)
+#extract vcf from gvcf
+print('extract vcf from gvcf')
+#gzip -dc ../../input/WGC081270U.g.vcf.gz | ../../programs/gvcftools-0.16/bin/extract_variants | bgzip -c > WGC081270U.vcf.gz
+command = """gzip -dc %s | %s/extract_variants | bgzip -c > %s.vcf.gz
+""" % (vcf_file, gvcftools_path, output_base)
+output = call(command, shell=True)
+print(output)
 
-# #index with tabix
-# command = """tabix -p vcf %s.vcf.gz""" % (output_base)
-# output = call(command, shell=True)
-# print(output)
+#index with tabix
+command = """tabix -p vcf %s.vcf.gz""" % (output_base)
+output = call(command, shell=True)
+print(output)
 
-# print('vcftools stats')
-# #vcftools metrics
-# command = """%s/perl/vcf-stats %s.vcf.gz > %s.vcftools.stats.txt
-# """ % (vcftools_path, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+print('vcftools stats')
+#vcftools metrics
+command = """%s/perl/vcf-stats %s.vcf.gz > %s.vcftools.stats.txt
+""" % (vcftools_path, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# #*coverage
-# #bcftools metrics
-# print('bcftools stats')
-# command = "%s/bcftools stats %s.vcf.gz > %s.bcftools.stats.txt" % (bcftools_path, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+#*coverage
+#bcftools metrics
+print('bcftools stats')
+command = "%s/bcftools stats %s.vcf.gz > %s.bcftools.stats.txt" % (bcftools_path, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# #snpeff
-# #extract vcf
-# print('extract vcf')
-# command = "bgzip -d -c %s.vcf.gz > %s.vcf" % (output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+#snpeff
+#extract vcf
+print('extract vcf')
+command = "bgzip -d -c %s.vcf.gz > %s.vcf" % (output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# print('snpeff')
-# command = "java -Xmx5g -jar %s/snpEff.jar eff -stats %s.snpeff.full.html -i vcf GRCh37.75 %s.vcf > %s.snpeff.full.vcf" % (snpeff_path, output_base, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+print('snpeff')
+command = "java -Xmx5g -jar %s/snpEff.jar eff -stats %s.snpeff.full.html -i vcf GRCh37.75 %s.vcf > %s.snpeff.full.vcf" % (snpeff_path, output_base, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# print('filtering')
-# #filtering VCF
-# command = "%s/bcftools filter -T %s %s.vcf > %s.filtered.exons.vcf" % (bcftools_path, target_file, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+print('filtering')
+#filtering VCF
+command = "%s/bcftools filter -T %s %s.vcf > %s.filtered.exons.vcf" % (bcftools_path, target_file, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# #filtered.exons.q50.dp50.vcf
-# command = "%s/bcftools filter -T %s -i'QUAL>50 && FMT/DP>50' %s.vcf > %s.filtered.exons.q50.dp50.vcf" % (bcftools_path, target_file, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+#filtered.exons.q50.dp50.vcf
+command = "%s/bcftools filter -T %s -i'QUAL>50 && FMT/DP>50' %s.vcf > %s.filtered.exons.q50.dp50.vcf" % (bcftools_path, target_file, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# #WGC081270U.filtered.exons.q100.dp100.vcf
-# command = "%s/bcftools filter -T %s -i'QUAL>100 && FMT/DP>100' %s.vcf > %s.filtered.exons.q100.dp100.vcf" % (bcftools_path, target_file, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+#WGC081270U.filtered.exons.q100.dp100.vcf
+command = "%s/bcftools filter -T %s -i'QUAL>100 && FMT/DP>100' %s.vcf > %s.filtered.exons.q100.dp100.vcf" % (bcftools_path, target_file, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# command = "java -Xmx5g -jar %s/snpEff.jar eff -stats %s.snpeff.exons.html -i vcf GRCh37.75 %s.filtered.exons.vcf > %s.snpeff.exons.vcf" % (snpeff_path, output_base, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+command = "java -Xmx5g -jar %s/snpEff.jar eff -stats %s.snpeff.exons.html -i vcf GRCh37.75 %s.filtered.exons.vcf > %s.snpeff.exons.vcf" % (snpeff_path, output_base, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
-# print('snpeff')
-# command = "java -Xmx5g -jar %s/snpEff.jar eff -stats %s.snpeff.exons.q100.dp100.html -i vcf GRCh37.75 %s.filtered.exons.q100.dp100.vcf > %s.snpeff.exons.q100.dp100.vcf" % (snpeff_path, output_base, output_base, output_base)
-# output = call(command, shell=True)
-# print(output)
+print('snpeff')
+command = "java -Xmx5g -jar %s/snpEff.jar eff -stats %s.snpeff.exons.q100.dp100.html -i vcf GRCh37.75 %s.filtered.exons.q100.dp100.vcf > %s.snpeff.exons.q100.dp100.vcf" % (snpeff_path, output_base, output_base, output_base)
+output = call(command, shell=True)
+print(output)
 
 print('bcftools stats')
 command = "%s/bcftools stats %s.filtered.exons.q100.dp100.vcf > %s.bcftools.stats.exons.q100.dp100.txt" % (bcftools_path, output_base, output_base)
