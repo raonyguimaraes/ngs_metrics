c7c4124bc6d4e9f233b42152a8ab4dbb97ebb10a
Raony Guimarães <raonyguimaraes@gmail.com>
2016-09-15 09:29:57 +0200

    DepthOfCoverage

--- bam_exome_metrics.py
+++ bam_exome_metrics.py
@@ -24,41 +24,113 @@ base_name = os.path.splitext(base)[0]
 print(base_name)
 
 
+human_reference = "/home/ubuntu/projects/input/b37/human_g1k_v37.fasta"
+
 output_folder = "/home/ubuntu/projects/output/reports/bam"
+samtools_dir = "/home/ubuntu/projects/programs/samtools-1.3.1"
+
+gatk_dir = "/home/ubuntu/projects/programs/gatk"
+
 #s3
 #if bam file start with s3 download from s3
 
 #fastqc
-#done!
+#done already!
 
-#featurecounts
-
-#Usage: featureCounts [options] -a <annotation_file> -o <output_file> input_file1 [input_file2] ...
-
-## Required arguments:
-  # -a <string>         Name of an annotation file. GTF/GFF format by default.
-  #                     See -F option for more formats.
-
-  # -o <string>         Name of the output file including read counts. A separate
-  #                     file including summary statistics of counting results is
-  #                     also included in the output (`<string>.summary')
-
-  # input_file1 [input_file2] ...   A list of SAM or BAM format files.
-
+#featureCounts
 command = """/home/ubuntu/projects/programs/subread-1.5.1-Linux-x86_64/bin/featureCounts -T 4 -p \
 -a /home/ubuntu/projects/input/gtf/Homo_sapiens.GRCh37.75.gtf \
--o %s/%s \
+-o %s/%s.featureCounts \
 %s""" % (output_folder, base_name, bam_file)
 # output = call(command, shell=True)
 # print(output)
 
 #bamtools
 command = """/home/ubuntu/projects/programs/bamtools/bin/bamtools stats -in %s""" % (bam_file)
+# output = call(command, shell=True)
+# print(output)
+
+#gatk DepthOfCoverage
+command = """
+java -Xmx15g -Djava.io.tmpdir=/projects/tmp -jar %s/GenomeAnalysisTK.jar -T DepthOfCoverage \
+-I %s \
+-R %s \
+-o %s/%s.DepthOfCoverage \
+-L %s \
+-log %s/%s.DepthofCoverage.log \
+""" % (gatk_dir, bam_file, human_reference, output_folder, base_name, target_file, output_folder, base_name)
 output = call(command, shell=True)
 print(output)
 
-#gatk
 #picard
+      
+#CollectAlignmentSummaryMetrics
+command = """
+java -jar -Xmx40g %s/CollectAlignmentSummaryMetrics.jar \
+I=%s \
+O=%s.AlignmentSummaryMetrics \
+R=%s \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, input_file, filename, reference)
+os.system(command)
+
+#CollectGcBiasMetrics
+command = """
+java -jar %s/CollectGcBiasMetrics.jar \
+R=%s \
+I=%s \
+O=%s.b37_1kg.GcBiasMetrics \
+CHART=%s.b37_1kg.GcBiasMetrics.pdf \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, reference, input_file, filename, filename)
+os.system(command)
+
+#CollectInsertSizeMetrics
+command = """
+java -jar %s/CollectInsertSizeMetrics.jar \
+I=%s \
+O=%s.b37_1kg.CollectInsertSizeMetrics \
+H=%s.b37_1kg.CollectInsertSizeMetrics.pdf \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, input_file, filename, filename)
+os.system(command)
+
+#MeanQualityByCycle
+command = """
+java -jar %s/MeanQualityByCycle.jar \
+I=%s \
+O=%s.b37_1kg.MeanQualityByCycle \
+CHART=%s.b37_1kg.MeanQualityByCycle.pdf \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, input_file, filename, filename)
+os.system(command)
+
+#QualityScoreDistribution
+command = """
+java -jar %s/QualityScoreDistribution.jar \
+I=%s \
+O=%s.b37_1kg.QualityScoreDistribution \
+CHART=%s.b37_1kg.QualityScoreDistribution.pdf \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, input_file, filename, filename)
+os.system(command)
+
+#BamIndexStats
+command = """
+java -jar %s/BamIndexStats.jar \
+INPUT=%s \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, input_file)
+os.system(command)
+
+
+
+
+#CalculateHsMetrics WholeGenome or CalculateHsMetrics ????
+command = """
+java -jar -Xmx4g %s/CalculateHsMetrics.jar \
+INPUT=%s \
+OUTPUT=%s.b37_1kg.HsMetrics \
+BAIT_INTERVALS=%s \
+TARGET_INTERVALS=%s \
+VALIDATION_STRINGENCY=LENIENT """ % (pic_dir, input_file, filename, exon, exon)
+#os.system(command)
+
+
 #qualimap
 #samtools flagstat
 
