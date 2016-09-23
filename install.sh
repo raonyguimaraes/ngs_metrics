3c1fca8b58e06cdaa42bb88da5fc12ff342dbaf2
Raony Guimaraes <raonyguimaraes@gmail.com>
2016-09-23 13:24:37 +0100

    bam metrics

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ sudo apt-get -y upgrade
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+sudo apt-get install s3cmd
+mkdir /home/ubuntu/projects/input
+mkdir /home/ubuntu/projects/input/bam
+mkdir /home/ubuntu/projects/output
+mkdir /home/ubuntu/projects/output/bam
+mkdir /home/ubuntu/projects/input/gtf
+mkdir /home/ubuntu/projects/input/grch37
