b05573f268ce3d8db3dd350e23ca521a8de6c391
Raony Guimarães <raonyguimaraes@gmail.com>
2016-09-27 16:27:34 +0100

    last changes

--- bam/bam_pipeline.py
+++ bam/bam_pipeline.py
@@ -9,20 +9,22 @@ import logging
 import argparse
 import datetime
 
+parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", help="BAM files (can be the location on S3)", nargs='+')
 parser.add_argument("-n", "--cores", help="Number of Cores to use")
 
+args = parser.parse_args()
+
 bam_files = args.input
 n_cores = int(args.cores)
 
+# log_file = "/home/ubuntu/projects/output/logs/run.%s.log.txt" % (str(datetime.datetime.now()).replace(' ', '_'))
 
-log_file = "/home/ubuntu/projects/output/logs/run.%s.log.txt" % (str(datetime.datetime.now()).replace(' ', '_'))
-
-
-logging.basicConfig(filename=log_file,level=logging.DEBUG)
+# logging.basicConfig(filename=log_file,level=logging.DEBUG)
 
 def run_command(command):
+    # print(command)
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while True:
         output = process.stdout.readline()
@@ -30,14 +32,13 @@ def run_command(command):
             break
         if output:
             # print(output.strip())
-            logging.info(output.strip())
+            # logging.info(output.strip())
     rc = process.poll()
     return rc
 
 def fastqc(bam_file):
     command = "python bam_fastqc.py -i %s" % (bam_file)
-    # run_command(command)
-    print command
+    run_command(command)
     # return x*x
 
 if __name__ == '__main__':
